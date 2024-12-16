434623bc0bc37e7f214f91f8917f24b21efd5aa1
Sergei_Piloyan <sergei.piloyan0302@gmail.com>
2024-12-15 18:51:04 -0800

    [SERVER]: add user service for registration

---
+++ server/service/user-service.js
@@ -0,0 +1,35 @@
+import userModel from "../models/user-model.js";
+import bcrypt from "bcrypt";
+import { v4 } from "uuid";
+import mailService from "./mail-service.js";
+import tokenService from "./token-service.js";
+import UserDto from "../dtos/user-dto.js";
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await userModel.findOne({ email });
+
+    if (candidate) {
+      throw new Error("User with this email already exist");
+    }
+
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = v4();
+    const user = await userModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+
+    await mailService.sendActivationMail(email, activationLink);
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+}
+
+export default new UserService();
